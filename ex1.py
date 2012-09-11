26d7b563f2338ccf4f4a55a8447115670d116309
Kryptyk Fysh <github@kryptykfysh.co.uk>
2012-09-12 08:28:24 +1000

    Added old files

---
+++ ex1.py
@@ -0,0 +1,20 @@
+# ex1.py
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# 1. Make your script print another line.
+print 'This is me printing another line. Whee.'
+
+# 2. Make your script print only one of the lines.
+# Just comment out the ones you don't want?
+
+# 3. Put a '#' (octothorpe) character at the beginning of a line. 
+# What did it do? Try to find out what this character does.
+
+# Oops, I guess I jumped the gun on commenting out lines.
---
+++ ex3.py
@@ -0,0 +1,40 @@
+# ex3.py
+
+# Prints some text.
+print "I will now count my chickens:"
+
+# Show precedence of division over addition.
+print "Hens", 25 + 30 / 6
+# Show precedence of multiplication over subtraction and the modulus function over both.
+print "Roosters", 100 - 25 * 3 % 4
+
+#Prints some text.
+print "Now I will count the eggs:"
+
+# Further demonstration of operator precendence.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# The same line using floating point numbers.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+
+# Prints some text.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Shows blooen result of comparison operation.
+print 3 + 2 < 5 -7
+
+# Shows the result of the left side of the comparison.
+print "What is 3 + 2?", 3 + 2
+# Shows the result of the right side of the comparison.
+print "What is 5 - 7?", 5 - 7
+
+# Prints some text.
+print "Oh, that's why it's False."
+
+# Prints some text.
+print "How about some more?"
+
+# All positive integers are greater than negative integers.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
