2c4487b7e41e3817deb8861dda1bc710f092a503
Kryptyk Fysh <github@kryptykfysh.co.uk>
2012-09-13 21:00:44 +1000

    Exercise 8

---
+++ ex8.py
@@ -0,0 +1,24 @@
+# ex8.py
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't sing.",
+	"So I said goodnight."
+)
+
+# Extra Credit
+
+# Do your checks of your work, write down your mistakes, try not 
+# to make them on the next exercise.
+
+# Notice that the last line of output uses both single and double 
+# quotes for individual pieces. Why do you think that is?
+# There's a single quote in the string, so a single quoted string 
+# wouldn't work.
