fb71272cc2222b2666d1f2683c2b5ad305e61609
Kryptyk Fysh <github@kryptykfysh.co.uk>
2012-09-13 20:56:11 +1000

    Exercise 7

---
+++ ex7.py
@@ -0,0 +1,39 @@
+# ex7.py
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that mary went."
+print "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end, try removing ti to see what happens.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# Extra Credit
+# For these next few exercises, you will have the exact same extra credit.
+
+# Go back through and write a comment on what each line does.
+
+# Read each one backwards or out loud to find your errors.
+# From now on, when you make mistakes write down on a piece of paper 
+# what kind of mistake you made.
+
+# When you go to the next exercise, look at the last mistakes you 
+# made and try not to make them in this new one.
+
+# Remember that everyone makes mistakes. Programmers are like magicians 
+# who like everyone to think they are perfect and never wrong, but it's 
+# all an act. They make mistakes all the time.
